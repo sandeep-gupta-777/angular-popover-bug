f5f84200d45527124ed619afe50d3d4b5ea4a2f1
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-01-17 23:17:27 +0530

    minor unknown changes

--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -490,17 +490,20 @@ export class UtilityService {
       plotOptions: {
         series: {
           pointStart: startTime_ms, //Date.UTC(2010, 0, 2),
-          pointInterval: granularity_Ms//24*3600*1000  // one day
+          pointInterval: granularity_Ms//24*3600*1000  // one day,
+          label: {
+            enabled: false
+          }
         }
       },
 
-      series: [{
-        name: 'sandeep',
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-      }, {
-        name: 'gupta',
-        data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2]
-      }]
+      // series: [{
+      //   name: 'sandeep',
+      //   data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      // }, {
+      //   name: 'gupta',
+      //   data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2]
+      // }]
     };
 
     // let categoriesString = rawData.map((dataItem) => dataItem.labels);
