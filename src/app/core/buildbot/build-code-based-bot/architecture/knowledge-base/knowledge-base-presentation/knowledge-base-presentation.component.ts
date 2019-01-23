0b630f741b450a898e02534c3a6373fd1d0cf2a5
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-01-23 22:43:21 +0530

    fix: handsontable: when chaging type to handsontable, update was throwing error

--- src/app/core/buildbot/build-code-based-bot/architecture/knowledge-base/knowledge-base-presentation/knowledge-base-presentation.component.ts
+++ src/app/core/buildbot/build-code-based-bot/architecture/knowledge-base/knowledge-base-presentation/knowledge-base-presentation.component.ts
@@ -96,6 +96,7 @@ export class KnowledgeBasePresentationComponent extends ModalImplementer impleme
     this.activatedRoute.queryParamMap.subscribe((queryParamMap: ParamMap) => {
       this.ner_id = (<any>queryParamMap).params['ner_id'];
     });
+    debugger;
   }
 
   openDeleteModal(template: TemplateRef<any>) {
@@ -115,6 +116,7 @@ export class KnowledgeBasePresentationComponent extends ModalImplementer impleme
 
   updateOrSaveConcept() {
 
+    debugger;
     let codeTextFromEditor;
     if (this.ner_type === 'regex') {
       if (!this.codeTextOutPutFromCodeEditor) {
@@ -128,8 +130,6 @@ export class KnowledgeBasePresentationComponent extends ModalImplementer impleme
           this.utilityService.showErrorToaster(`Syntax is not valid. ${this.ner_type} only accespts Array literal`);
           return;
         }
-
-
 
         codeTextFromEditor = JSON.parse(this.codeTextOutPutFromCodeEditor);
       } catch (e) {
