a17f3f1f783a30eecbb3ea3f0bc132d62ac18575
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-02-21 23:27:17 +0530

    commit marker

--- src/app/core/buildbot/build-code-based-bot/architecture/pipeline/pipeine-id-to-pipeline-module-wrapper.pipe.ts
+++ src/app/core/buildbot/build-code-based-bot/architecture/pipeline/pipeine-id-to-pipeline-module-wrapper.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {Select} from '@ngxs/store';
 import {Observable} from 'rxjs';
 import {IAppState} from '../../../../../ngxs/app.state';
@@ -13,18 +13,19 @@ export class PipeineIdToPipelineModuleWrapperPipe implements PipeTransform {
   @Select() app$: Observable<IAppState>;
   pipelineModulesV2List: IPipelineItemV2[];
   pipeline_modules = [];
-  constructor(){
-    this.app$.pipe(take(1)).subscribe((appState)=>{
+
+  constructor() {
+    this.app$.pipe(take(1)).subscribe((appState) => {
       this.pipelineModulesV2List = appState.pipelineModulesV2List;
-    })
+    });
   }
 
   transform(id: any): any {
     debugger;
-    return this.pipelineModulesV2List.find((wrapper)=>{
-      return !!wrapper.pipeline_modules.find((module)=>{
+    return this.pipelineModulesV2List.find((wrapper) => {
+      return !!wrapper.pipeline_modules.find((module) => {
         return module.id === id;
-      })
-    })
+      });
+    });
   }
 }
