e16d629b0a2846188954fe1fbb0be7d6bc2d02be
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-02-21 21:08:43 +0530

    fix: consumers: decyption: consumers decryption wasnt working properly

--- src/app/core/bot-detail/consumers/consumers.component.ts
+++ src/app/core/bot-detail/consumers/consumers.component.ts
@@ -127,12 +127,12 @@ export class ConsumersComponent extends MaterialTableImplementer implements OnIn
         const url = this.constantsService.getBotConsumerByIdUrl(this.consumerItemToBeDecrypted.id);
         this.serverService
           .makeGetReq<IConsumerItem>({url, headerData: {'bot-access-token': this.bot.bot_access_token}})
-          .pipe(map((result) => {
-            const modified_update_at = (new Date(result.updated_at)).toDateString();
-            return {...result, updated_at: modified_update_at};
-          }))
-          .subscribe((value: IConsumerItem) => {
-            this.consumersDecrypted = value;
+          // .pipe(map((result) => {
+          //   const modified_update_at = (new Date(result.updated_at)).toDateString();
+          //   return {...result, updated_at: modified_update_at};
+          // }))
+          .subscribe((value: {objects:IConsumerItem[]}) => {
+            this.consumersDecrypted = value.objects[0];
             const index = this.consumerItems.findIndex((value) => value.id === this.consumerItemToBeDecrypted.id);
             this.consumerItems[index] = this.consumersDecrypted;
             this.tableData = this.initializeTableData(this.consumerItems, this.getTableDataMetaDict());
@@ -152,7 +152,7 @@ export class ConsumersComponent extends MaterialTableImplementer implements OnIn
     const url = this.constantsService.getBotConsumerByIdUrl(data['id']);
     this.serverService
       .makeGetReq<IConsumerItem>({url, headerData: {'bot-access-token': this.bot.bot_access_token}})
-      .subscribe(({objects}) => {
+      .subscribe(({objects, meta}) => {
         debugger;
         // let index = ObjectArrayCrudService.getObjectIndexByKeyValuePairInObjectArray(this.consumerItems, {id: consumer.id});
         // if(index >= 0){
@@ -160,7 +160,7 @@ export class ConsumersComponent extends MaterialTableImplementer implements OnIn
         // }else {
         //   this.consumerItems.push(consumer);
         // }
-        // this.totalRecords = 1;
+        this.totalRecords = meta.total_count;
         this.tableData = this.initializeTableData(objects, this.getTableDataMetaDict());
       });
   }
