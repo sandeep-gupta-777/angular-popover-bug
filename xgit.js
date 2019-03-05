51344c3f0e8c75ea7fb219746a08dc31b09ef00d
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-03-05 23:04:26 +0530

    start: xgit

--- xgit.js
+++ xgit.js
@@ -11,9 +11,9 @@ async function runCommand(str) {
 async function f() {
     let currentBranch = await runCommand(`git rev-parse --abbrev-ref HEAD`);
     console.log(currentBranch);
-    let lastCommit = await runCommand(`git log -1`);
+    let lastCommit = await runCommand(`git log -1 -s --format=%ct`);
     fs.writeFileSync('D:\\nodebook\\DEVELOP\\bot_platform-fe\\src\\git-info.json',
-        JSON.stringify({currentBranch,lastCommit}));
+        JSON.stringify({currentBranch,lastCommit: Number(lastCommit)*1000}));
 }
 f();
 
