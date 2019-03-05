629222a547fd78cd18a371c199538e24bd8fa0d9
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-03-06 00:13:51 +0530

    feature: update to latest version

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -719,7 +719,7 @@ export class ServerService {
 
   compareDeployDates(){
     let lastDeployed_Cache = deploy_obj_botplateform_fe.lastDeploy;
-    this.makeGetReq({url:'/deploy.json'})
+    this.makeGetReq({url:'/static/deploy.json'})
       .subscribe((value:{"currentBranch":string,"lastDeploy":number})=>{
         let lastDeployed_api = value.lastDeploy;
         debugger;
