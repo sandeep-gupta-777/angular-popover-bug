c7d70df08481077f401f1472950b1e35fe325a02
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-03-06 15:36:46 +0530

    xgit changed path

--- xgit.js
+++ xgit.js
@@ -15,25 +15,28 @@ async function f() {
     let lastCommit = await runCommand(`git log -1 -s --format=%ct`);
     let time = Date.now();
     console.log("=========XGIT:START===========");
+    const deploy_json_path = path.join(__dirname, 'src/deploy.json');
+    const deploy_js_path = path.join(__dirname, 'src/assets/js/deploy.js');
     try {
         console.log("=========BEFORE===========");
-        console.log("deploy.json::",fs.readFileSync(path.join(__dirname, 'src/deploy.json')).toString());
-        console.log("deploy.js::",fs.readFileSync(path.join(__dirname, 'src/assets/js/deploy.js')).toString());
+        console.log("deploy.json::",fs.readFileSync(deploy_json_path).toString());
+        console.log("deploy.js::",fs.readFileSync(deploy_js_path).toString());
     }catch (e) {
         console.log(e);
-        console.log("deploy.json path::",path.join(__dirname, 'src/deploy.json'));
-        console.log("deploy.js path::",path.join(__dirname, 'src/assets/js/deploy.js'));
+    }finally {
+        console.log("deploy.json path::",deploy_json_path);
+        console.log("deploy.js path::",deploy_js_path);
     }
     console.log(`Writing variable time = ${time}`);
 
     try {
-        fs.writeFileSync('src\\deploy.json', JSON.stringify({currentBranch,lastDeploy: time}));
-        fs.writeFileSync('src\\assets\\js\\deploy.js',
+        fs.writeFileSync(deploy_json_path, JSON.stringify({currentBranch,lastDeploy: time}));
+        fs.writeFileSync(deploy_js_path,
             `var deploy_obj_botplateform_fe = {"currentBranch":"develop","lastDeploy":${time}};`);
 
         console.log("=========AFTER===========");
-        console.log("deploy.json::",fs.readFileSync('src\\deploy.json').toString());
-        console.log("deploy.js::",fs.readFileSync('src\\assets\\js\\deploy.js').toString());
+        console.log("deploy.json::",fs.readFileSync(deploy_json_path).toString());
+        console.log("deploy.js::",fs.readFileSync(deploy_js_path).toString());
     }catch (e) {
         console.log(e);
     }
