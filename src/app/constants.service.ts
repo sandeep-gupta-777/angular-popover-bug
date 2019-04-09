d86cce8a35103d083336088aee8532163ac6d7ab
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-04-09 12:33:36 +0530

    bot versioning url cjanged from bot id to vversion id

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -429,7 +429,7 @@ export class ConstantsService {
     return this.BACKEND_URL + 'api/v1/botversioning/?limit=1000'; //"http://localhost:8000/api/v1/botversioning"
   }
 
-  getSaveVersionByBotId(id) {
+  getSaveVersionByVersionId(id) {
     return this.BACKEND_URL + `api/v1/botversioning/${id}/`; //https://dev.imibot.ai/api/v1/botversioning/9/
   }
 
