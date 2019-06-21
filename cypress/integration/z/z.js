1216622f3d38667ac70ccbd88b06d73b8b6e1452
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-06-21 11:07:24 +0530

    removed a test case

--- cypress/integration/z/z.js
+++ cypress/integration/z/z.js
@@ -5,6 +5,6 @@ describe('zzz wait for long time', function() {
         // console.log(cy.fixture('post-login-data.json'));
         // cy.login_localstorage();
         // cy.reload();
-        cy.wait(10000000000);
+        // cy.wait(10000000000);
     })
 });
