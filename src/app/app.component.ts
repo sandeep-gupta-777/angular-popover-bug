5836e48e3eddaf1912312b0f2606c6539e33f66b
Sandeep Kumar Gupta <sandeepkgupta007@gmail.com>
2019-07-04 13:50:44 +0530

    fix: chat preview related fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router){}
   ngOnInit() {
     // console.info('App bootstrap success!');
+    alert();
   }
 
   ngAfterViewInit() {
