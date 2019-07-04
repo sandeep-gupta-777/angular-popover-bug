d5b28a974ef67d3e11cf24c629968499b9ac3fb3
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-07-04 16:35:08 +0530

    done removing aleart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent implements OnInit {
   constructor(private router: Router){}
   ngOnInit() {
     // console.info('App bootstrap success!');
-    alert();
   }
 
   ngAfterViewInit() {
