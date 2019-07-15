adffb3d7acd7dccfbfabf9ac130681e9b044fa0a
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-07-15 14:14:12 +0530

    wired

--- src/app/core/bot-detail/curation/curation-settings/curation-settings.component.ts
+++ src/app/core/bot-detail/curation/curation-settings/curation-settings.component.ts
@@ -18,9 +18,8 @@ export class CurationSettingsComponent implements OnInit {
     private formBuilder: FormBuilder,
   ) { }
   @Input() bot: IBot;
-  curationSettingsForm : FormGroup;
+  @Input() curationSettingsForm : FormGroup;
   ngOnInit() {    
-    this.curationSettingsForm = this.makeCurationSettingsForm();
     this.curationSettingsForm.get('allow_curation').valueChanges
     .subscribe((val)=>{
       if(!val){
@@ -40,21 +39,5 @@ export class CurationSettingsComponent implements OnInit {
       }
     })
   }
-  makeCurationSettingsForm(){
-    let form = this.formBuilder.group({
-      "allow_curation" : [this.bot.allow_curation],
-      "curation_settings": this.formBuilder.group({
-        "agent_handover": this.formBuilder.group({"enabled":[this.bot.curation_settings.agent_handover.enabled]}),
-        "downvoted": this.formBuilder.group({"enabled":[this.bot.curation_settings.downvoted.enabled]}),
-        "fallback": this.formBuilder.group({"enabled":[this.bot.curation_settings.fallback.enabled]}),
-        "from_session": this.formBuilder.group({"enabled":[this.bot.curation_settings.from_session.enabled]}),
-        "low_confidence": this.formBuilder.group({
-          "enabled":[this.bot.curation_settings.low_confidence.enabled],
-          "low_confidence_score": [this.bot.curation_settings.low_confidence.low_confidence_score]
-        }),
-        "partial_match": this.formBuilder.group({"enabled":[this.bot.curation_settings.partial_match.enabled]}),
-      })
-    });
-    return form;
-  }
+  
 }
