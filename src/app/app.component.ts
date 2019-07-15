7c6d3813c1bfb1a56c9a984f4c0ae2dcc298b9b8
Sandeep Kumar Gupta <sandeepkgupta007@gmail.com>
2019-07-15 19:48:34 +0530

    fix: sw fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,20 @@ declare var CodeMirror: any;
 export class AppComponent implements OnInit {
   title = 'app';
   loading;
-  message
+  message;
   constructor(private router: Router, private messagingService: MessagingService) {
   }
 
   ngOnInit() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('../firebase-messaging-sw.js')
+        .then(function(registration) {
+          console.log('Registration successful, scope is:', registration.scope);
+        }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+    }
+
     console.info('App bootstrap success!');
 
     const userId = 'user001asdasdasd';
