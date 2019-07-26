d7a7ff3c7456ab45f438f06111c51e119f92d61a
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-07-26 12:38:32 +0530

    fixed filter fro moverview issue

--- src/app/core/bot-detail/curation/curation-filter/curation-filter.component.ts
+++ src/app/core/bot-detail/curation/curation-filter/curation-filter.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Input, ViewChild, EventEmitter, Output, AfterViewInit, OnDestroy, AfterViewChecked} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm,FormGroup, FormControl } from '@angular/forms';
 import {ConstantsService} from "../../../../constants.service";
 
 @Component({
@@ -7,33 +7,19 @@ import {ConstantsService} from "../../../../constants.service";
   templateUrl: './curation-filter.component.html',
   styleUrls: ['./curation-filter.component.scss']
 })
-export class CurationFilterComponent implements OnInit,AfterViewInit {
+export class CurationFilterComponent implements OnInit {
 
   constructor(
     private constantsService : ConstantsService,
   ) { }
   @Input() triggered_rules : string[];
+  @Input() form : FormGroup;
   @Output() formSubmitted = new EventEmitter();
   @Output() clearForm = new EventEmitter();
   @Input() unsolved : boolean;
-  @Input() set resolveIssuesOfArticleByCount(count:number){
-    if(count){
-      this.curationForm.reset();
-      debugger;
-      this.curationForm.form.patchValue({
-        "order_by": "group_by_section",
-        "issue_count_filter": 'issue_count_per_section',
-        "count": count
-      })
-      // this.submitedForm();
-      // setTimeout(()=>{
-      //   debugger;
-      //   this.submitedForm()},10)
-    }
-  }
-  @ViewChild('filterForm') curationForm: NgForm;
-  maxDate = new Date();
-  date = {};
+
+  // @ViewChild('filterForm') curationForm: NgForm;
+
   formChangesSubscription;
   ngOnInit() {
     
@@ -53,59 +39,19 @@ export class CurationFilterComponent implements OnInit,AfterViewInit {
   }
 
   submitedForm(){
-    let body = {};
-
-    for (var key in this.curationForm.value) {
-      if (this.curationForm.value[key]) {
-        body[key] = this.curationForm.value[key];
-      }
-    }
-    if(body['updated_at__range'] && Object.keys(body['updated_at__range']).length > 0 ){
-      body['updated_at__range'] = body['updated_at__range']["begin"].getTime()+','+(body['updated_at__range']["end"].getTime()+86340000);
-    }else{
-      delete body['updated_at__range'];
-    }
-
-    if(body['issue_count_filter'] && body['count']){
-      body[body['issue_count_filter']]  = body['count'];
-      delete body['issue_count_filter'];
-      delete body['count'];
-    }else{
-      delete body['issue_count_filter'];
-      delete body['count'];
-    }
-    
-    if(body['order_by']){
-      body['order_by'] = `-${body['order_by']}`;
-    }else{
-      body['order_by'] = `-updated_at`;
-    }
-    if(this.unsolved){
-      delete body['hideIgnored'];
-    }else{
-      if(body['hideIgnored']){
-        body['curation_state__in']="resolved";
-        delete body['hideIgnored'];
-      }else{
-        body['curation_state__in']="resolved,ignored";
-        delete body['hideIgnored'];
-      }
-    }
-    
-    this.formSubmitted.emit(body);
+    this.formSubmitted.emit({'unsolved':this.unsolved,value:this.form.value});
   }
-
   clearFormClicked(){
-    this.curationForm.reset();
+    this.form.reset();
     
     if(this.unsolved){
-      this.curationForm.form.patchValue({
+      this.form.patchValue({
         'order_by' : `updated_at`
       })
       this.submitedForm();
     }
     if(!this.unsolved){
-      this.curationForm.form.patchValue({
+      this.form.patchValue({
         'curation_state__in':"resolved,ignored",
         'order_by' : `updated_at`
       })
@@ -120,17 +66,4 @@ export class CurationFilterComponent implements OnInit,AfterViewInit {
   //   this.submitedForm();
     
   // }
-  ngAfterViewInit(){
-    debugger;
-    setTimeout(()=>{
-      this.curationForm.form.get('order_by').valueChanges
-      .subscribe((val)=>{
-        debugger;
-        if(val){
-          debugger;
-          this.submitedForm();
-        }
-      }),0})
-    
-  }
 }
