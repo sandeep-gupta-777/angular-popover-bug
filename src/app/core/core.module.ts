9d5324905fd0abd9963fce903952761f9c317296
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-08-16 11:39:01 +0530

    merged gentemp to dev and fixed some issues

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -56,7 +56,6 @@ import {LazyLoadImageModule} from 'ng-lazyload-image';
 import {ScriptsLoadResolver} from '../script-load.resolver';
 import {BotAccessTokenResolver} from '../bot-access-token.resolver';
 import {BotResolver} from '../bot.resolver';
-import {HnResolver} from '../core.resolver';
 import { GentemplateSidebarComponent } from './buildbot/build-code-based-bot/architecture/code/code-input/code-gentemplate-ui-wrapper/gentemplate-sidebar/gentemplate-sidebar.component';
 
 
