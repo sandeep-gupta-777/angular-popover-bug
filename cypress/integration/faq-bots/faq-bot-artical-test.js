48c4131002ebe42c4e04a1f29ca36a53a15df770
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2019-08-27 18:20:39 +0530

    article gen temp test

--- cypress/integration/faq-bots/faq-bot-artical-test.js
+++ cypress/integration/faq-bots/faq-bot-artical-test.js
@@ -2,34 +2,40 @@
 
 describe('faq-artical-tab', function () {
 
-    beforeEach(() => {
-        cy.login_UI();
-    });
+  beforeEach(() => {
+    cy.login_UI();
+  });
 
-    it('checks if artical is present or not',function(){
-        cy.contains('Search Based').click();
-        cy.url().should('include', 'viewbots?type=faqbot');
-        cy.wait(3000);
-        cy.get('.grid-bot-preview-name').first().click()
-        cy.wait(3000);
-        cy.url().should('include', '/botdetail/faqbot/')
+  it('checks if artical is present or not', function () {
+    cy.contains('Smart bots').click();
+    cy.contains('Q&A bots').click();
+    cy.url().should('include', 'viewbots?type=faqbot');
+    cy.wait(3000);
+    cy.get('.grid-bot-preview-name').first().click()
+    cy.wait(3000);
+    cy.url().should('include', '/botdetail/faqbot/')
 
-        cy.contains('Article').click()
-        cy.wait(30000);
-        cy.url().should('include','build=articles')
-        cy.contains('All articles')
-        cy.contains('Knowledge base')
+    cy.contains('Article').click()
+    cy.wait(5000);
+    cy.url().should('include', 'build=articles')
+    cy.contains('All articles')
+    cy.contains('Knowledge base')
 
-        cy.get('.article-item').first().click()
+    cy.get('.article-item').first().click()
 
-        cy.get('.category-lable')
-            .contains('Category')
+    cy.get('.category-lable')
+      .contains('Category')
 
-        cy.get('.questions-heading')
-            .contains('Questions')
-        cy.get('.response-heading')
-            .contains('Response')
+    cy.get('.questions-heading')
+      .contains('Questions')
+    cy.get('.response-heading')
+      .contains('Response')
 
-    })
+    cy.get('[data-cy=gentemp-sidebar-text]').click();
+    cy.get('[data-cy=gentemp-sidebar-carousel]').click();
+    cy.get('[data-cy=gentemp-sidebar-quick-reply]').click();
+
+
+  })
 
 })
