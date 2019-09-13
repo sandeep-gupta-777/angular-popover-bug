7d18dccc55be1e2f04d6be6e5fa4cc5baadbff00
Sandeep Kumar Gupta <sandeepkgupta007@gmail.com>
2019-09-14 00:28:35 +0530

    fix: lang param implemented and some fixes

--- src/app/core/buildbot/build-code-based-bot/bot-config/bot-config.service.ts
+++ src/app/core/buildbot/build-code-based-bot/bot-config/bot-config.service.ts
@@ -81,7 +81,7 @@ export class BotConfigService {
   }
 
   getBasicInfoForm(bot: IBot) {
-
+    debugger;
     this.basicInfoForm = this.formBuilder.group({
       name: [bot.name, Validators.required],
       bot_unique_name: [bot.bot_unique_name, Validators.required],
