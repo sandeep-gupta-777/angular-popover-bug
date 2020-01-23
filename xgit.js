f5c049178b063ed25ee5940d21892719d0d41364
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-01-23 19:52:38 +0530

    fix: added xgit file

---
+++ xgit.js
@@ -0,0 +1,19 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+
+
+async function runCommand(str) {
+  // console.log((await exec(str)).stdout);
+  return (await exec(str)).stdout.trim();
+}
+
+async function f() {
+  let currentBranch = await runCommand(`git rev-parse --abbrev-ref HEAD`);
+  console.log(currentBranch);
+  let lastCommit = await runCommand(`git log -1 -s --format=%ct`);
+  fs.writeFileSync('D:\\nodebook\\DEVELOP\\bot_platform-fe\\src\\git-info.json',
+    JSON.stringify({currentBranch,lastCommit: Number(lastCommit)*1000}));
+}
+f();
+
