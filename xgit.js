48d86f1eff49e716db04efcdd4d936325b0342c5
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-01-23 20:05:06 +0530

    fix: modified xgit

--- xgit.js
+++ xgit.js
@@ -12,7 +12,7 @@ async function f() {
   let currentBranch = await runCommand(`git rev-parse --abbrev-ref HEAD`);
   console.log(currentBranch);
   let lastCommit = await runCommand(`git log -1 -s --format=%ct`);
-  fs.writeFileSync('D:\\nodebook\\DEVELOP\\bot_platform-fe\\src\\git-info.json',
+  fs.writeFileSync('D:\\nodebook\\DEVELOP\\imi\\src\\deploy.json',
     JSON.stringify({currentBranch,lastCommit: Number(lastCommit)*1000}));
 }
 f();
