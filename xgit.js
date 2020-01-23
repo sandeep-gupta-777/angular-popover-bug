e17b7a5ed6995ded55d9e857d584380b93bca7f3
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-01-23 23:02:59 +0530

    fix: xgit

--- xgit.js
+++ xgit.js
@@ -11,7 +11,7 @@ async function f() {
   let currentBranch = await runCommand(`git rev-parse --abbrev-ref HEAD`);
   let lastDeploy = await runCommand(`git log -1 -s --format=%ct`);
   lastDeploy =  Number(lastDeploy) * 1000;
-  const obj = {currentBranch, lastDeploy: Number(lastDeploy) * 1000}
+  const obj = {currentBranch, lastDeploy: lastDeploy};
   fs.writeFileSync('src\\assets\\js\\deploy.json', JSON.stringify(obj));
   fs.writeFileSync('src\\assets\\js\\deploy.js', `var deploy_obj_botplateform_fe = {"currentBranch":"${currentBranch}","lastDeploy":${lastDeploy}};`);
   console.dir(obj);
