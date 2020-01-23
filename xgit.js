d8b5c2931c7b5d35a3968d0c949f240afb10a42e
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-01-23 20:22:02 +0530

    fix: xgit

--- xgit.js
+++ xgit.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 
 
 async function runCommand(str) {
-  // console.log((await exec(str)).stdout);
   return (await exec(str)).stdout.trim();
 }
 
 async function f() {
   let currentBranch = await runCommand(`git rev-parse --abbrev-ref HEAD`);
-  console.log(currentBranch);
   let lastCommit = await runCommand(`git log -1 -s --format=%ct`);
   fs.writeFileSync('D:\\nodebook\\DEVELOP\\imi\\src\\deploy.json',
-    JSON.stringify({currentBranch,lastCommit: Number(lastCommit)*1000}));
+    JSON.stringify({currentBranch, lastCommit: Number(lastCommit) * 1000}));
+  console.log(currentBranch);
+  console.log(lastCommit);
 }
+
 f();
 
